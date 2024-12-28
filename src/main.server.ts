17b3b78ee2acf269ac3be58b2896c83c83fa4db0
Albin David C <albindavidc007@gmail.com>
2024-12-28 09:39:51 +0530

    updated

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,12 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
-import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
-const bootstrap = () =>
-  bootstrapApplication(AppComponent, {
-    ...config,
-    providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
-  });
+const bootstrap = () => bootstrapApplication(AppComponent, config);
 
 export default bootstrap;
