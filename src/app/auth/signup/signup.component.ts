5df140da6e1e61498e32d92c7f36aa3873b43ca3
Albin David C <albindavidc007@gmail.com>
2025-01-07 19:32:32 +0530

    created signup view and logic

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrl: './signup.component.css',
 })
 export class SignupComponent {
+  message: string = '';
+  constructor(private authService: AuthService) {}
 
+  onSignup(form: NgForm): void {
+    if (form.valid) {
+      this.authService
+        .signup(form.value)
+        .subscribe((msg) => (this.message = msg));
+    }
+  }
 }
