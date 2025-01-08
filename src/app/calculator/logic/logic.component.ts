87ff1b1ca50b9eaadfe3730d4872b1cd6f9a9741
Albin David C <albindavidc007@gmail.com>
2025-01-08 21:10:53 +0530

    created changing colors for the equalto button

--- src/app/calculator/logic/logic.component.ts
+++ src/app/calculator/logic/logic.component.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
 import { DisplayComponent } from '../display/display.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-logic',
-  imports: [DisplayComponent],
+  imports: [DisplayComponent, CommonModule],
   templateUrl: './logic.component.html',
   styleUrl: './logic.component.css',
 })
 export class LogicComponent {
   displayedValue: string = '0';
-  previousValue:string = '0';
+  previousValue: string = '0';
   currentValue: number | null = null;
   operation: string | null = null;
   isNewInput: boolean = true;
+
+  newColors: string[] = ['blue', 'green', 'red', 'yellow', 'purple'];
+  currentColorIndex: number = 0;
 
   onDigitClick(digit: string) {
     if (this.isNewInput) {
@@ -36,7 +40,7 @@ export class LogicComponent {
       this.currentValue! /= currentNumber;
     }
 
-    this.previousValue =this.currentValue!.toString();
+    this.previousValue = this.currentValue!.toString();
     this.displayedValue = this.currentValue!.toString();
   }
 
@@ -51,13 +55,12 @@ export class LogicComponent {
     this.isNewInput = true;
   }
 
-  onEqualsTo(){
-    if(this.operation && this.currentValue !== null){
+  onEqualsTo() {
+    if (this.operation && this.currentValue !== null) {
       this.calculate();
       this.currentValue = null;
       this.operation = null;
     }
-
   }
 
   onClear() {
@@ -67,9 +70,21 @@ export class LogicComponent {
     this.isNewInput = true;
   }
 
-  onDisplayValueChange(newValue:string){
+  onDisplayValueChange(newValue: string) {
     this.displayedValue = newValue;
   }
 
+  changeColor(): void {
+    this.currentColorIndex =
+      (this.currentColorIndex + 1) % this.newColors.length;
+  }
 
+  get changeColorClass(): string {
+    return this.newColors[this.currentColorIndex];
+  }
+
+  changeColorAndEqualTo(): void {
+    this.onEqualsTo();
+    this.changeColor();
+  }
 }
